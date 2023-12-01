98fa474f5407dfccde950fba18d01ed719f9ebb2
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-12-01 16:49:37 -0400

    Update course.py

--- App/models/course.py
+++ App/models/course.py
@@ -8,6 +8,7 @@ class Course(db.Model):
     courseTitle = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     credits = db.Column(db.Integer)
+    type = db.Column(db.string(5))
     grade = db.Column(db.Float)
     semester = db.Column(db.Integer)
     year = db.Column(db.Integer)
@@ -17,11 +18,12 @@ class Course(db.Model):
     prereq = db.relationship('Prerequisites', backref='course', lazy=True)
 
     
-    def __init__(self, code, title, credits, grade, semester, year, complete, prereq):
+    def __init__(self, code, title, credits, grade, type,  semester, year, complete, prereq):
         self.id = code
         self.courseTitle = title
         self.credits = credits
         self.grade = grade
+        self.type = type
         self.semester = semester
         self.year = year
         self.complete = complete
@@ -33,6 +35,7 @@ class Course(db.Model):
             'Course Name: ': self.courseTitle,
             'Course Grade: ': self.grade,
             'No. of Credits: ': self.credits,
+            'Course Type: ': self.type,
             'Semester: ': self.semester,
             'Year: ': self.year,
             'Completed?': self.complete,
