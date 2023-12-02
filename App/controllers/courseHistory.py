5b309e270a3785deda77aa68f8008fbb174fd217
SanjayB1704 <146902451+SanjayB1704@users.noreply.github.com>
2023-12-02 11:58:02 -0400

    Update courseHistory.py

--- App/controllers/courseHistory.py
+++ App/controllers/courseHistory.py
@@ -25,6 +25,42 @@ def update_course_history(course_history_id, new_course_id, new_student_id):
     else:
         return jsonify({'message': 'Course history not found'}), 404
 
+def add_course_to_course_history(student_id, course_id, grade):
+    # Check if the course is already in the CourseHistory for the student
+    existing_entry = CourseHistory.query.filter_by(student_id=student_id, course_id=course_id).first()
+
+    if existing_entry:
+        return jsonify({'message': 'Course already exists in the student\'s CourseHistory'}), 400
+
+    # Create a new entry in the CourseHistory
+    new_course_history_entry = CourseHistory(
+        student_id=student_id,
+        course_id=course_id,
+        grade=grade  # You can include the grade if available
+    ) 
+
+
+def getCompletedCourseCodes(student_id):
+    completed_courses = CourseHistory.query.filter_by(student_id=student_id).all()
+
+    course_ids = [course.course_id for course in completed_courses]
+
+    return jsonify({'course_ids': course_ids}), 200
+
+def getCompletedCourseCodesAndGrade(student_id):
+    completed_courses = CourseHistory.query.filter_by(student_id=student_id).all()
+
+    courses_data = []
+
+    for course in completed_courses:
+        course_data = {
+            'course_id': course.course_id,
+            'grade': course.grade  # Assuming there's a 'grade' attribute in your CourseHistory model
+        }
+        courses_data.append(course_data)
+
+    return jsonify({'courses_and_grades': courses_data}), 200
+
 def delete_course_history(course_history_id):
     course_history = CourseHistory.query.get(course_history_id)
     if course_history:
