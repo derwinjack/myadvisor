9b8fc4e42e1e861773fe0540886e248aa8a660c7
danieldcaesar <39540283+danieldcaesar@users.noreply.github.com>
2023-12-03 23:56:09 -0400

    Update courseHistory.py

--- App/models/courseHistory.py
+++ App/models/courseHistory.py
@@ -3,4 +3,4 @@ from App.database import db
 class CourseHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
