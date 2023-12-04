48f0f9cea5e84e4ed05b545593c88327f95c8b39
danieldcaesar <39540283+danieldcaesar@users.noreply.github.com>
2023-12-03 23:54:22 -0400

    Update semester.py

--- App/models/semester.py
+++ App/models/semester.py
@@ -1,11 +1,14 @@
 from App.database import db
 
+
 class Semester(db.Model):
     __tablename__ = 'semesters'
     id = db.Column(db.Integer, primary_key=True)
     semester_title = db.Column(db.String(2))
 
-    course_plan = db.relationship('CoursePlan', back_populates='sem_id')
+    course_plans = db.relationship('CoursePlan', backref='semesters', lazy=True)
 
-    def __init__(self, sem_title):
+    def __init__(self, sem_id, sem_title):
+        self.id = sem_id
         self.semester_title = sem_title
+        
