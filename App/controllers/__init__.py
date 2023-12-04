e8caa89e4d4df489a47cef3034ed4d51a12f182b
danieldcaesar <39540283+danieldcaesar@users.noreply.github.com>
2023-12-04 10:10:19 -0400

    Update __init__.py

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,12 +1,11 @@
 from .user import *
+from .auth import *
 from .course import *
 from .program import *
 from .staff import *
 from .student import *
-from .prerequisites import *
+from .prerequistes import *
 from .coursePlan import *
 from .department import *
-from .semester import *
-from .studentCourse import *
-from .courseProgram import *
-from .courseHistory import *
+from .courseHistory import*
+from.semester import*
