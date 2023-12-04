cae42eded194b20a97e84b819b76612afde951cc
danieldcaesar <39540283+danieldcaesar@users.noreply.github.com>
2023-12-03 23:58:37 -0400

    Update student.py

--- App/models/student.py
+++ App/models/student.py
@@ -2,15 +2,17 @@ from App.models import User
 from App.database import db
 
 class Student(User):
+    __tablename__ = 'students'
     id = db.Column(db.String(10), db.ForeignKey('user.id'), primary_key=True)
     name = db.Column(db.String(50))
     gpa = db.Column(db.Float) 
     program_id = db.Column(db.ForeignKey('programs.id')) 
     
-    prog = db.relationship('Program', back_populates='stu')
+    prog = db.relationship('Program', back_populates='students')
+    #course_plans = db.relationship('CoursePlan', back_populates='student', uselist=True)
+    course_plan = db.relationship('CoursePlan', back_populates= 'students', uselist=False)
     
-    course_plan = db.relationship('CoursePlan', back_populates='student', uselist=False)
-
+    
 
     def __init__(self, name, password, program_id):
         super().__init__(name, password)
