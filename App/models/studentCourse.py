ae0a72840a0f305e4dff8cd7aa9141361a4b19ce
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-12-04 22:02:24 -0400

    Update studentCourse.py

--- App/models/studentCourse.py
+++ App/models/studentCourse.py
@@ -5,5 +5,5 @@ from App.models import Course
 class StudentCourse(db.Model):
     __tablename__='student_course'
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    #course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
