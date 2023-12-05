fc593479b4fb005581e3cdb41117e2985e7d9b6b
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-12-04 21:54:30 -0400

    Update prerequistes.py

--- App/controllers/prerequistes.py
+++ App/controllers/prerequistes.py
@@ -1,19 +1,33 @@
 from App.models import Prerequisites
 from App.database import db
+from App.models.course import Course
 
-def create_prereq(prereqCode, courseName):
-    prereq = Prerequisites(prereqCode, courseName)
+def create_prereq(prereqCode, courseCode):
+    prereq = Prerequisites(prereqCode, courseCode)
     db.session.add(prereq)
     db.session.commit()
 
-def get_all_prerequisites(courseName):
-    return Prerequisites.query.filter(Prerequisites.courseName == courseName).all()
+#def get_all_prerequisites(courseCode):
+    #course = courseCode
+    #prereq = course.prereq
+    #return prereq
 
-def getPrereqCodes(courseName):
-    prereqs = get_all_prerequisites(courseName)
-    codes = []
+def get_all_prerequisites( courseCode):
+    
 
-    for p in prereqs:
-        codes.append(p.prereq_courseCode)
+    course = Course.query.get(courseCode)
+
+    if course:
+        prerequisites = course.prereq  # Accessing the prereq attribute
+        return prerequisites
+    else:
+        return None
+
+
+
+def getPrereqCodes(courseCode):
+    prereqs = get_all_prerequisites(courseCode)
+    codes = prereqs
+
     
     return codes
